59b40037b128810307292e12b93b7166d7ec8a5f
Mario23junior <mariojunior3251@gmail.com>
2021-06-08 04:00:52 -0300

    Iniciando api de listagem de cometas

---
+++ src/main/java/com/project/cometa/CometaApplication.java
@@ -0,0 +1,13 @@
+package com.project.cometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CometaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cometa/CometaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cometa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CometaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
