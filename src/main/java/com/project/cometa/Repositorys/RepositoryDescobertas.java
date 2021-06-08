e6cb8e297983b01b2cd861b48d34281f8eb2b14c
Mario23junior <mariojunior3251@gmail.com>
2021-06-08 07:48:39 -0300

    Criando repository da tabela Descoberta

---
+++ src/main/java/com/project/cometa/Repositorys/RepositoryDescobertas.java
@@ -0,0 +1,9 @@
+package com.project.cometa.Repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.cometa.Model.Descobertas;
+
+public interface RepositoryDescobertas extends JpaRepository<Descobertas, Long>{
+
+}
