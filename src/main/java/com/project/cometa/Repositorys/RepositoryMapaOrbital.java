3aae8ccf69f7897ae1530fc00b5a7e502b760353
Mario23junior <mariojunior3251@gmail.com>
2021-06-08 07:55:51 -0300

    Criando repository de MapaOrbital

---
+++ src/main/java/com/project/cometa/Repositorys/RepositoryMapaOrbital.java
@@ -0,0 +1,9 @@
+package com.project.cometa.Repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.cometa.Model.MapaOrbital;
+
+public interface RepositoryMapaOrbital extends JpaRepository<MapaOrbital, Long>{
+
+}
