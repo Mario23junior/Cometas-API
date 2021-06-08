e809985c066daf4ec876b09cc3c62265b35e2cc0
Mario23junior <mariojunior3251@gmail.com>
2021-06-08 07:53:38 -0300

    Criando repository de InformacoesOrbitais

---
+++ src/main/java/com/project/cometa/Repositorys/RepositoryInformacaosOrbitas.java
@@ -0,0 +1,9 @@
+package com.project.cometa.Repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.cometa.Model.InformacaosOrbitais;
+
+public interface RepositoryInformacaosOrbitas extends JpaRepository<InformacaosOrbitais, Long>{
+
+}
