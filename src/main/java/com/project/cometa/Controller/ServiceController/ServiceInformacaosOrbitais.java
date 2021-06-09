f8f402f3eef9dda4fc1a9a44d63d20e3972ff31c
Mario23junior <mariojunior3251@gmail.com>
2021-06-09 06:18:59 -0300

    Listando as informacaoes orbitas do cometa

--- src/main/java/com/project/cometa/Controller/ServiceController/ServiceInformacaosOrbitais.java
+++ src/main/java/com/project/cometa/Controller/ServiceController/ServiceInformacaosOrbitais.java
@@ -1,7 +1,10 @@
 package com.project.cometa.Controller.ServiceController;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.project.cometa.Exception.ResourceNotFoundExceptionIntercept;
 import com.project.cometa.Model.InformacaosOrbitais;
 import com.project.cometa.Repositorys.RepositoryInformacaosOrbitas;
 
@@ -17,5 +20,11 @@ public class ServiceInformacaosOrbitais {
 	public InformacaosOrbitais salvarInformacoes(InformacaosOrbitais informacaosOrbitais) {
 		InformacaosOrbitais saveInfoOrb = repositoryInformacaosOrbitas.save(informacaosOrbitais);
 		return saveInfoOrb;
+	}
+	
+	public Optional<InformacaosOrbitais> listByComet(Long id) {
+		Optional<InformacaosOrbitais> list = repositoryInformacaosOrbitas.findById(id);
+		list.orElseThrow(() -> new ResourceNotFoundExceptionIntercept("informação não encontrada"));
+		return list;
 	}
 }
