9c069357e5f500ddfd287f22a2e6660fd0f70683
Mario23junior <mariojunior3251@gmail.com>
2021-06-10 05:32:03 -0300

    criando modelo de dados DTO e entidade DescobertasDTO

---
+++ src/main/java/com/project/cometa/DTOModel/DescobertasDTO.java
@@ -0,0 +1,43 @@
+package com.project.cometa.DTOModel;
+
+public class DescobertasDTO {
+   
+	private Long id;
+	private String imagen;
+    private String nome;
+    private String designacao;
+	private String coDeNome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDesignacao() {
+		return designacao;
+	}
+	public void setDesignacao(String designacao) {
+		this.designacao = designacao;
+	}
+	public String getCoDeNome() {
+		return coDeNome;
+	}
+	public void setCoDeNome(String coDeNome) {
+		this.coDeNome = coDeNome;
+	}
+	
+	
+}
