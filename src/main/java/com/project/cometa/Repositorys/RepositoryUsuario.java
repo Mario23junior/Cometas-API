3f12c4e92e98df74519d35e75df610497e874335
Mario23junior <mariojunior3251@gmail.com>
2021-06-11 05:28:25 -0300

    criando base de dados da tebela usuarios e repositery

---
+++ src/main/java/com/project/cometa/Repositorys/RepositoryUsuario.java
@@ -0,0 +1,9 @@
+package com.project.cometa.Repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.cometa.Model.Usuario;
+
+public interface RepositoryUsuario extends JpaRepository<Usuario, Long>{
+
+}
