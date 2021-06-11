12cab71c11389eb65397d3080be4103a4c31dac5
Mario23junior <mariojunior3251@gmail.com>
2021-06-11 09:50:08 -0300

    Configurando urls de permisao de acesso e realizando comparacao com dados cadastrados e fazendo login

--- src/main/java/com/project/cometa/Config/SpringSecurity.java
+++ src/main/java/com/project/cometa/Config/SpringSecurity.java
@@ -1,6 +1,8 @@
 package com.project.cometa.Config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -24,5 +26,25 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter{
 		    .userDetailsService(usuarioServiceImplement)
 		    .passwordEncoder(configurationPasswordEncoder.passwordEncoder());
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http 
+		   .csrf().disable()
+		   .authorizeRequests()
+		       .antMatchers("/v1/api/informcaosOrbitais/**")
+	               .hasAnyRole("USER","ADMIN")
+		        .antMatchers("/v1/api/mapaOrbital/**")
+	               .hasAnyRole("USER","ADMIN")
+		         .antMatchers("/v1/api/propriendadesFisicas/**")
+	               .hasAnyRole("USER","ADMIN")
+		         .antMatchers("/v1/api/listAll/**")
+	               .hasAnyRole("USER","ADMIN")
+		         .antMatchers(HttpMethod.POST,"/api/v1/user/**")
+		           .permitAll()
+		           .anyRequest().authenticated()
+		         .and()
+		           .httpBasic();
+  	}
 	
 }
